0a4619ab5d4ee98a8db7f6a601a684ae3f003936
MaicolRuiz01 <maicolandreyruga@ufps.edu.co>
2023-06-26 12:12:10 -0500

    se agregaron las entidades usuario y su controlador, tambien se agrego la entidad Rol y rolUsuario con sus respectivos repositorios y coontroladores

---
+++ src/main/java/link/softbond/repositorios/RolRepository.java
@@ -0,0 +1,13 @@
+package link.softbond.repositorios;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import link.softbond.entities.Rol;
+
+public interface RolRepository extends JpaRepository<Rol, Integer>{
+	
+	
+
+}
