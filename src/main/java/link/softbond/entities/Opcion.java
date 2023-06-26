200261b9dc8e3e96438127a4f9c7bf6db1450f1f
Hael1230 <102492080+Hael1230@users.noreply.github.com>
2023-06-26 10:53:29 -0500

    Commit 1
    Creación entities Opcion

---
+++ src/main/java/link/softbond/entities/Opcion.java
@@ -0,0 +1,34 @@
+package link.softbond.entities;
+
+import java.io.Serializable;
+import javax.persistence.*;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
+import lombok.Data;
+
+import java.sql.Timestamp;
+import java.util.List;
+
+@Entity
+@Data
+public class Opcion{
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Integer id;
+
+    @OneToOne
+    @JoinColumn(name="id")
+    private Consulta consulta;
+
+    @OneToOne
+    @JoinColumn(name="id")
+    private Examen examen;
+
+    private Timestamp fecha;
+
+    @OneToOne
+    @JoinColumn(name="id")
+    private Usuario usuario;
+}
