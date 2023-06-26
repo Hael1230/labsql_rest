0460f8924bf909a082b8fb8a44026ffdca370027
MaicolRuiz01 <maicolandreyruga@ufps.edu.co>
2023-06-26 12:36:24 -0500

    Se agrego el metodo listar de usuariorol

--- src/main/java/link/softbond/controller/UsuarioController.java
+++ src/main/java/link/softbond/controller/UsuarioController.java
@@ -1,10 +1,13 @@
 package link.softbond.controller;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -41,6 +44,24 @@ public class UsuarioController {
 			// TODO: handle exception
 			return ResponseEntity.status(HttpStatus.CONFLICT).build();
 		}
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<?> findByIdUsuario(@PathVariable Integer id){
+		
+		try {
+			Optional<Usuario> UsuarioCurrent=usuarioRepository.findById(id);
+			
+			if(UsuarioCurrent.isPresent()) {
+				return ResponseEntity.ok(UsuarioCurrent);
+			}
+			return ResponseEntity.status(HttpStatus.CONFLICT).build();
+		} catch (Exception e) {
+			// TODO: handle exception
+			return ResponseEntity.status(HttpStatus.CONFLICT).build();
+		}
+		
+		
 	}
 
 }
